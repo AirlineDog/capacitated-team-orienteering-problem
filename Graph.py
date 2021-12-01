8b92b187e3c93760008b14a4de5a92a419cf1e9f
AirlineDog <t8190042@aueb.gr>
2021-12-01 18:31:09 +0000

    graphic representation

---
+++ Graph.py
@@ -0,0 +1,16 @@
+from Model import *
+import matplotlib.pyplot as plt
+
+m = Model()
+m.build_model()
+plt.figure(figsize=(8, 6), dpi=80)
+xs = [x.x for x in m.allNodes]
+ys = [y.y for y in m.allNodes]
+ids = [x.id for x in m.allNodes]
+plt.scatter(xs, ys, c="blue", marker=".")
+plt.scatter(23.142, 11.736, c="black")
+for x, y, id in zip(xs, ys, ids):
+    plt.annotate(str(id), xy=(x,y))
+plt.gcf().set_size_inches((20, 20))
+plt.savefig("mygraph.png")
+print()
